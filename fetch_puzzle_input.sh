a7410e2c4b2a697dd114a3d35733e832f96bc2cf
Florian Wörister <31357619+fwoerister@users.noreply.github.com>
2024-12-07 09:27:20 +0100

    fetch puzzle input updated

--- fetch_puzzle_input.sh
+++ fetch_puzzle_input.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-day=`expr $(date +"%d")`
-year=`expr $(date +"%Y")`
+day=$1
+year=$(date +"%Y")
 
-printf -v fday "%02d" $day
+printf -v fday "%02d" "$day"
 
 cookies="session=$SESSION"
 target="https://adventofcode.com/$year/day/$day/input"
 
-
-curl --cookie "$cookies" $target > "input/day$fday.txt"
+curl --cookie "$cookies" "$target" > "input/day$fday.txt"
