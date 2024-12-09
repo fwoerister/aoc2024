12af55214920b7518d3574c79486378b9f3e02f7
Florian Wörister <florian.woerister@univie.ac.at>
2024-12-09 18:32:12 +0100

    run all script added

---
+++ run_all.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+days="1 2 3 4 5 6 7 8 9"
+
+source .env
+export SESSION=$SESSION
+
+for day in $days
+do
+  printf -v fday "%02d" "$day"
+  echo "##############"
+  echo "# Run Day $fday #"
+  echo "##############"
+
+  ./fetch_puzzle_input.sh "$day"
+
+  python "day$fday.py" "input/day$fday.txt"
+
+  echo ""
+done
