af77f862f11ae2a3f0f2b0e812192d2222bd6879
Florian Wörister <florian.woerister@univie.ac.at>
2024-12-09 17:44:57 +0100

    clean up day09 solution

--- day09.py
+++ day09.py
@@ -1,81 +1,97 @@
+from functools import reduce
 from time import time
 
 from util.args import parse_args
 from util.submit import submit_answer
 
 
-def calculate_checksum(diskmap: str):
-    if len(diskmap) % 2 == 0:
-        diskmap = diskmap[:-1]
-    diskmap = [int(b) for b in list(diskmap)]
-
-    disk = []
-    b_id = 0
-
-    idx = 0
-    while idx < len(diskmap):
-        if idx % 2 == 0:
-            disk.extend([b_id] * diskmap[idx])
-            b_id += 1
-        else:
-            disk.extend([-1] * diskmap[idx])
-        idx += 1
-
-    free_idx = disk.index(-1)
-    data_idx = len(disk) - 1
-
-    while disk.count(-1) > 0 and free_idx < data_idx:
-        if disk[data_idx] == -1:
-            data_idx -= 1
-        else:
-            disk[free_idx] = disk[data_idx]
-            disk[data_idx] = -1
-
-            free_idx = disk.index(-1)
-            data_idx -= 1
-
-    idx = 0
+def calculate_checksum(disk):
     checksum = 0
-    while idx < len(disk):
+    for idx in range(len(disk)):
         if disk[idx] != -1:
             checksum += idx * disk[idx]
-        idx += 1
-
     return checksum
 
 
-def calculate_checksum2(diskmap: str):
+def diskmap_to_int(diskmap):
     if len(diskmap) % 2 == 0:
         diskmap = diskmap[:-1]
-    diskmap = [int(b) for b in list(diskmap)]
+    return [int(b) for b in list(diskmap)]
+
+
+def defrag_memory_level1(diskmap: str):
+    diskmap = diskmap_to_int(diskmap)
+    disk = [0] * diskmap[0]
+
+    free_block_idx = 1
+    data_block_idx = len(diskmap) - 1
+
+    relocated_blocks = 0
+
+    while free_block_idx < data_block_idx:
+
+        free_block_size = diskmap[free_block_idx]
+        data_block_size = diskmap[data_block_idx]
+
+        if (relocated_blocks + data_block_size) < free_block_size:
+            disk.extend([data_block_idx // 2] * data_block_size)
+            relocated_blocks += data_block_size
+            data_block_idx -= 2
+        else:
+            disk.extend([data_block_idx // 2] * (free_block_size - relocated_blocks))
+            diskmap[data_block_idx] -= free_block_size - relocated_blocks
+            relocated_blocks += free_block_size - relocated_blocks
+
+            free_block_idx += 2
+            relocated_blocks = 0
+
+            disk.extend([free_block_idx // 2] * diskmap[free_block_idx - 1])
+
+    return disk
+
+
+def defrag_memory_level2(diskmap: str):
+    diskmap = diskmap_to_int(diskmap)
     disk = []
 
-    idx = 0
-    while idx < len(diskmap):
+    for idx in range(len(diskmap)):
         if idx % 2 == 0:
-            disk.append([[idx // 2] * diskmap[idx]])
+            disk.append([idx // 2] * diskmap[idx])
         else:
             disk.append([])
 
-    for data_block in range(len(diskmap) - 1, 0, -2):
-        for free_block in range(1, len(diskmap), 2):
-            if len(disk[data_block]) <= len(disk[free_block]):
-                disk[free_block].extend(disk[data_block])
-                disk[data_block] = []
+    for data_block_idx in range(len(diskmap) - 1, 0, -2):
+        for free_block_idx in range(1, data_block_idx):
+            free_block_size = diskmap[free_block_idx] - len(disk[free_block_idx])
+            data_block_size = diskmap[data_block_idx]
+
+            if data_block_size <= free_block_size:
+                disk[free_block_idx].extend(disk[data_block_idx])
+                disk[data_block_idx] = [-1] * diskmap[data_block_idx]
+                break
+
+    for free_block_idx in range(1, len(disk), 2):
+        disk[free_block_idx].extend([-1] * (diskmap[free_block_idx] - len(disk[free_block_idx])))
+
+    disk = reduce(lambda x, y: x + y, disk)
+
+    return disk
 
 
 if __name__ == '__main__':
     args = parse_args()
-    answer_1 = 0
-    answer_2 = 0
 
     with args.puzzle_input as file:
         disk_map = file.readlines()[0].strip()
         start = round(time() * 1000)
 
-        answer_1 = calculate_checksum(disk_map)
+        disk_level1 = defrag_memory_level1(disk_map)
+        answer_1 = calculate_checksum(disk_level1)
 
         end_1 = round(time() * 1000)
+
+        disk_level2 = defrag_memory_level2(disk_map)
+        answer_2 = calculate_checksum(disk_level2)
 
         end_2 = round(time() * 1000)
 
