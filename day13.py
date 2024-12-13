9c976c88f61d70583566eab90844d3cb49fd812d
Florian Wörister <florian.woerister@univie.ac.at>
2024-12-13 17:02:13 +0100

    refactor day13

--- day13.py
+++ day13.py
@@ -1,4 +1,3 @@
-import functools
 import re
 from time import time
 import sympy
@@ -11,19 +10,37 @@ def get_machine_configs(file):
     line = file.readline()
     configs = []
     while line:
-        a_x, a_y = [int(val) for val in re.findall(r'\+(\d*)', line)]
+        button_a_x, button_a_y = [int(val) for val in re.findall(r'\+(\d*)', line)]
         line = file.readline()
-        b_x, b_y = [int(val) for val in re.findall(r'\+(\d*)', line)]
+        button_b_x, button_b_y = [int(val) for val in re.findall(r'\+(\d*)', line)]
         line = file.readline()
         price_x, price_y = [int(val) for val in re.findall(r'=(\d*)', line)]
         line = file.readline()
 
-        configs.append((a_x, a_y, b_x, b_y, price_x, price_y))
+        configs.append({
+            'ax': button_a_x,
+            'ay': button_a_y,
+            'bx': button_b_x,
+            'by': button_b_y,
+            'px': price_x,
+            'py': price_y,
+        })
 
         while line and line == '\n':
             line = file.readline()
 
     return configs
+
+
+def get_minimal_tokens(c):
+    a, b = sympy.symbols('a, b', real=True, positive=True, integer=True)
+    eq = [
+        sympy.Eq((a * c['ax'] + b * c['bx']), c['px']),
+        sympy.Eq((a * c['ay'] + b * c['by']), c['py']),
+    ]
+
+    s = sympy.solve(eq)
+    return s[a] * 3 + s[b] if s else 0
 
 
 if __name__ == '__main__':
@@ -37,29 +54,16 @@ if __name__ == '__main__':
         start = round(time() * 1000)
 
         for config in configs:
-            a, b = sympy.symbols('a, b', real=True, positive=True, integer=True)
-            equations = [
-                sympy.Eq((a * config[0] + b * config[2]), config[4]),
-                sympy.Eq((a * config[1] + b * config[3]), config[5]),
-            ]
-
-            solution = sympy.solve(equations)
-            if solution:
-                answer_1 += solution[a] * 3 + solution[b]
+            answer_1 += get_minimal_tokens(config)
 
         end_1 = round(time() * 1000)
 
         answer_2 = 0
-        for config in configs:
-            a, b = sympy.symbols('a, b', real=True, positive=True, integer=True)
-            equations = [
-                sympy.Eq((a * config[0] + b * config[2]), config[4] + 10000000000000),
-                sympy.Eq((a * config[1] + b * config[3]), config[5] + 10000000000000),
-            ]
 
-            solution = sympy.solve(equations)
-            if solution:
-                answer_2 += solution[a] * 3 + solution[b]
+        for config in configs:
+            config['px'] += 10000000000000
+            config['py'] += 10000000000000
+            answer_2 += get_minimal_tokens(config)
 
         end_2 = round(time() * 1000)
 
