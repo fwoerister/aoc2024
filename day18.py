00aa65322d868455346a1a238827ba99a54cdd1c
Florian Wörister <ffwoerister@gmail.com>
2024-12-18 07:25:47 +0100

    day 18 added

---
+++ day18.py
@@ -0,0 +1,80 @@
+from heapq import heappush, heappop
+from time import time
+
+from util.args import parse_args
+from util.datastructures import Grid
+from util.submit import submit_answer
+
+SIZE = 71
+BYTES_FALLEN = 1024
+
+
+class Memory(Grid):
+    def __init__(self, width, height, byte_pos):
+        rows = ['.' * width] * height
+        super().__init__(rows)
+
+        self.start = (0, 0)
+        self.end = (width - 1, height - 1)
+        self.byte_pos = byte_pos
+
+    def find_shortest_path_after(self, num_bytes):
+        byte_pos = set(self.byte_pos[:num_bytes])
+
+        open_pos = []
+        heappush(open_pos, (0, self.start))
+        visited = {self.start}
+
+        while open_pos:
+            path_length, current = heappop(open_pos)
+
+            if current == self.end:
+                return path_length
+
+            neighbours = filter(lambda nb: nb not in byte_pos, self.get_neighbours(*current))
+
+            for n in neighbours:
+                if n not in visited:
+                    visited.add(n)
+                    heappush(open_pos, (path_length + 1, n))
+
+        return -1
+
+
+if __name__ == '__main__':
+    args = parse_args()
+
+    with args.puzzle_input as file:
+        byte_pos = []
+
+        for line in file:
+            x, y = line.strip().split(',')
+            byte_pos.append((int(x), int(y)))
+
+        start = round(time() * 1000)
+
+        mem = Memory(SIZE, SIZE, byte_pos)
+        answer_1 = mem.find_shortest_path_after(BYTES_FALLEN)
+
+    end_1 = round(time() * 1000)
+
+    mem = Memory(SIZE, SIZE, byte_pos)
+
+    for fallen_bytes in range(len(byte_pos) + 1):
+        if mem.find_shortest_path_after(fallen_bytes) == -1:
+            answer_2 = byte_pos[fallen_bytes - 1]
+            break
+
+    end_2 = round(time() * 1000)
+
+    print(answer_1)
+    print(f'time: {end_1 - start}ms')
+
+    print(answer_2)
+    print(f'time: {end_2 - end_1}ms')
+
+    if args.submit == 1:
+        print(submit_answer(answer_1, 18, 1))
+
+    if args.submit == 2:
+        print(submit_answer(answer_2, 18, 2))
--- run_all.sh
+++ run_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-days="1 2 3 4 5 6 7 8 9 10 11 12 13 14 15"
+days="1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18"
 
 source .env
 export SESSION=$SESSION
